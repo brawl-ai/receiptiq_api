9b2f4372e09424e9edfeb4a19baf35ed011fdd80
PeterKahenya <kahenya0@gmail.com>
2025-07-10 09:47:45 +0300

    secure download urls

--- api/files.py
+++ api/files.py
@@ -1,13 +1,16 @@
-from fastapi import APIRouter, HTTPException, status
+from typing import Tuple
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import FileResponse, RedirectResponse
 
+from utils import get_current_active_verified_user
+from models import User
 from utils import StorageService
 
 router = APIRouter(tags=["Files"])
 storage = StorageService()
 
 @router.get("/{file_path:path}", status_code=200)
-async def download(file_path: str) -> FileResponse:
+async def download(file_path: str, auth: Tuple[User, str] = Depends(get_current_active_verified_user)) -> FileResponse:
     try:
         download_url = storage.get_url(file_path)
         return RedirectResponse(url=download_url)
