2205fa408d77218f7c66778c36211352082d52a8
PeterKahenya <kahenya0@gmail.com>
2025-07-12 05:40:07 +0300

    set config defaults for easier testing

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 import logging
 from honeybadger import Honeybadger, contrib
 from pydantic_settings import BaseSettings
@@ -7,35 +8,35 @@ class Settings(BaseSettings):
     project_name: str = "ReceiptIQ API"
     version: str = "0.0.1"
     api_v1_str: str = "/api/v1"
-    secret_key: str
+    secret_key: str = ""
     algorithm: str = "HS256"
     access_token_expiry_seconds: int = 3600  # 1 hour
-    otp_length: int 
+    otp_length: int = 5 
     otp_expiry_seconds: int  = 300  # 5 minutes
     password_reset_token_length: int = 32
     password_reset_token_expiry_seconds: int = 300
     refresh_token_length: int = 128
     refresh_token_expiry_seconds: int = 2592000 # 30 days
     frontend_url: str = ""
-    postgres_user: str 
-    postgres_password: str 
-    postgres_db: str 
-    postgres_host: str 
-    postgres_port: str
-    honeybadger_api_key: str = None
-    openai_api_key: str = None
-    client_id: str = None
-    client_secret: str = None
-    resend_api_key: str
-    email_from: str
-    email_reply_to: str
-    paystack_secret_key: str
-    paystack_base_url: str
-    aws_access_key_id: str
-    aws_endpoint_url_s3: str
-    aws_region: str
-    aws_secret_access_key: str
-    bucket_name: str
+    postgres_user: str = ""
+    postgres_password: str = ""
+    postgres_db: str = ""
+    postgres_host: str = ""
+    postgres_port: str = "5842"
+    honeybadger_api_key: str = ""
+    openai_api_key: str = "testapi"
+    client_id: str = ""
+    client_secret: str = ""
+    resend_api_key: str = ""
+    email_from: str = ""
+    email_reply_to: str = ""
+    paystack_secret_key: str = ""
+    paystack_base_url: str = ""
+    aws_access_key_id: str = ""
+    aws_endpoint_url_s3: str = ""
+    aws_region: str = ""
+    aws_secret_access_key: str = ""
+    bucket_name: str = ""
     
     @property
     def database_url(self) -> str:
@@ -44,13 +45,14 @@ class Settings(BaseSettings):
 settings = Settings()
 
 logger = logging.getLogger('HB')
-hb = Honeybadger()
-hb.configure(environment=settings.environment, api_key=settings.honeybadger_api_key,force_report_data=True)
-hb_handler = contrib.HoneybadgerHandler(api_key=settings.honeybadger_api_key)
-hb_handler.honeybadger = hb
-hb_handler.setLevel(logging.INFO)
-hb_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-logger.addHandler(hb_handler)
+if len(settings.honeybadger_api_key) > 0:    
+    hb = Honeybadger()
+    hb.configure(environment=settings.environment, api_key=settings.honeybadger_api_key,force_report_data=True)
+    hb_handler = contrib.HoneybadgerHandler(api_key=settings.honeybadger_api_key)
+    hb_handler.honeybadger = hb
+    hb_handler.setLevel(logging.INFO)
+    hb_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(hb_handler)
 
 permissions = [
     ('Admin','admin'),
@@ -73,3 +75,6 @@ subscription_plans = [
     ('Standard Monthly','1k invoices per month',10.00, "KES","monthly",0,"ACTIVE"),
     ('Standard Annually','1.1k invoices per month',8, "KES","annually",0,"ACTIVE")
 ]
+
+def get_settings():
+    return Settings()
